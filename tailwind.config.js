bb20b7e0e2e67cb140ba484854d94b796032b6cd
Whitroy <55481516+Whitroy@users.noreply.github.com>
2021-06-17 11:23:07 +0530

    Implementaing Reward Page of Starbucks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    mode : "jit",
+    purge : ["./index.html"],
+    colors :{
+      primary:{
+        50 : "#ffffff",
+        100 : "#f1f8f6",
+        200 : "#f3f1e7",
+        300 : "#d4e9e2",
+        350 : "#019f59",
+        400 : "#008248",
+        500 : "#1e3932",
+        550 : "#686b62",
+        600 : "#000000"
+      }
+    },
     extend: {},
   },
   variants: {
